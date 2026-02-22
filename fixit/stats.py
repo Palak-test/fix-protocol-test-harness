9a5833751aa7c004e323aad93d8d4caaafd2b400
pari9p <behlpari4@gmail.com>
2026-02-22 11:32:03 +0530

    feat: add statistics module for message analysis

---
+++ fixit/stats.py
@@ -0,0 +1,11 @@
+# stats.py for Fixit
+
+def calculate_message_stats(messages):
+    """Calculate statistics for a list of FIX messages (dicts)."""
+    total = len(messages)
+    types = {}
+    for msg in messages:
+        msg_type = msg.get('35') if isinstance(msg, dict) else None
+        if msg_type:
+            types[msg_type] = types.get(msg_type, 0) + 1
+    return {'total': total, 'types': types}
